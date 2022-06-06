160dfac10c0c9c92dc7d6c2225825356270cd898
alimrankhan <alimrankhan54@gmail.com>
2022-06-06 17:41:33 +0600

    via gui

--- C. Most Similar Words.py
+++ C. Most Similar Words.py
@@ -2,6 +2,13 @@ t= int(input())
 while t>0:
     t=t-1
     n,m= map(int, input().split())
+    su= 0
+    se= set()
     for i in range(0,n):
-        pass
+        s= input()
+        su= 0
+        for i in s:
+            su= su+ ord(i)
 #ord('a') and chr(97)
+        se.add(su)
+    print(se)
